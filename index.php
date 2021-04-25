8c35f36224892175df3f3c0f69c6926225ad6f85
Utkarsh Jain <54702807+utkarsh22040@users.noreply.github.com>
2021-04-25 13:16:36 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
   
-  <title>SideBar Menu</title>
+  <title>Attendance Management System </title>
   <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
